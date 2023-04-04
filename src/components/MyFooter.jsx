6b848c61b5691a4669a6c03473208d04f221e091
Luigi160397 <luigi.difraia.97@gmail.com>
2023-04-04 17:22:05 +0200

    margin

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,9 +1,9 @@
 const MyFooter = () => (
-  <footer className="d-flex bg-dark">
+  <footer className="d-flex bg-dark mt-5">
     <span className="text-white m-auto">
       <strong>EPICODE</strong> - Copyright {new Date().getFullYear()}
     </span>
   </footer>
-)
+);
 
-export default MyFooter
+export default MyFooter;
